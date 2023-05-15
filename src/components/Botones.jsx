5ed26a148258db1727d764074e0443fbd4e4fb14
EricAtencio12 <74440555+EricAtencio12@users.noreply.github.com>
2023-05-14 23:03:12 -0500

    Modificaciones a las rutas

--- src/components/Botones.jsx
+++ src/components/Botones.jsx
@@ -8,12 +8,13 @@ const Botones = () => {
     <BrowserRouter>
     <div className='menu'>
         <h4>SELECCIONA EL TIPO DE CALCULADORA</h4>
-        <NavLink to='/edad' className="btn1">Calcula tu edad</NavLink>
-        <NavLink to='/peso' className="btn2">Evalua tu peso</NavLink>
+        <NavLink to='/calcula_edad/edad' className="btn1">Calcula tu edad</NavLink>
+        <NavLink to='/calcula_edad/peso' className="btn2">Evalua tu peso</NavLink>
     </div>
            <Routes>
-                <Route path='/edad' element={<Calculadora/>}></Route>
-                <Route path='/peso' element={<CalculaPeso/>}></Route>
+                <Route path='/calcula_edad/edad' element={<Calculadora/>}></Route>
+                <Route path='/calcula_edad/peso' element={<CalculaPeso/>}></Route>
+                <Route path='*' element={<Calculadora/>}></Route>
            </Routes>
     </BrowserRouter>
   )
